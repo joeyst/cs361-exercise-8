72648ad90a7922ef8497aa5de33c36cba407791a
joeyst <joeystenbeck@gmail.com>
2022-11-16 10:57:01 -0800

    Use message instead of direct reference to instance variable.

--- exercise-8.2.rb
+++ exercise-8.2.rb
@@ -86,17 +86,17 @@ class PlayGames
   def play()
     case game_number
     when 1
-      poker = Poker.new(@player_list)
-      poker.play_poker()
-      puts poker.get_results()
+      poker = Poker.new(player_list)
+      poker.play_poker
+      puts poker.get_results
     when 2
-      chess = Chess.new(@player_list)
-      chess.play_game()
-      puts chess.get_results()
+      chess = Chess.new(player_list)
+      chess.play_game
+      puts chess.get_results
     when 3
-      go = Go.new(@player_list)
-      go.play()
-      puts go.get_score()
+      go = Go.new(player_list)
+      go.play
+      puts go.get_score
     end
   end
 end
